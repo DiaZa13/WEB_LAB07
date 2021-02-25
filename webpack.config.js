5ab7c01a9742bb1a8d88c4faa68a78aaa33f8dc7
Zaray <cor191025@uvg.edu.gt>
2021-02-25 01:48:18 -0600

    adding file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     devtool:false,
     entry: {
         index: './src/index.js',
-        pag1: './src/pag1.js'
+        //pag1: './src/pag1.js'
     },
     output: {
         filename: "[name].bundle.js",
@@ -16,23 +16,35 @@ module.exports = {
     },
     //plugin
     plugins: [
-        new miniCssExtractplugin({ filename: "[name].bundle.css" }),
+        new miniCssExtractplugin({ filename: "index.bundle.css" }),
         new htmlWebpackplugin({
             template: "./src/index.html",
             filename: "index.html",
             chunks:["index"]
         }),
-        new htmlWebpackplugin({
+       /* new htmlWebpackplugin({
             template: "./src/pag1.html",
             filename: "pag1.html",
             chunks:["pag1"]
-        }),
+        }),*/
     ],
     module:{
         rules: [
             {
                 test: /\.scss$/,
                 use: [miniCssExtractplugin.loader,'css-loader','sass-loader'],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
             },
         ],
     },
